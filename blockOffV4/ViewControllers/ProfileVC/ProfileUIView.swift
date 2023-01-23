335aa5c50bc26836c50155d606abe3f0eeff0967
David Malicke <davemalicke@gmail.com>
2023-01-22 21:38:22 -0500

    Add Datepickers

--- blockOffV4/ViewControllers/ProfileVC/ProfileUIView.swift
+++ blockOffV4/ViewControllers/ProfileVC/ProfileUIView.swift
@@ -6,14 +6,28 @@
 //
 
 import SwiftUI
+import UIKit
+import EventKit
 
 struct ProfileUIView: View {
+    @State private var startTime = Date.now
+    @State private var endDate = Date.now
     
     var body: some View {
         Form {
             Section("Subscription") {
                 Text("Active")
             }
+            Section("Work Day") {
+                VStack(alignment: .leading) {
+                    DatePicker("Start Time", selection: $startTime, displayedComponents: .hourAndMinute)
+                    DatePicker("End Time", selection: $endDate, displayedComponents: .hourAndMinute)
+
+                }
+            }
+        }
+        .onAppear {
+            UIDatePicker.appearance().minuteInterval = 15
         }
     }
 }
