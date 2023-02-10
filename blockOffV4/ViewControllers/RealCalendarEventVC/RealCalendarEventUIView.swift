0536ae7c5784050e9253401289fe251657e3feb6
David Malicke <davemalicke@gmail.com>
2023-02-09 21:56:30 -0500

    Continue building RealCalendarView

--- blockOffV4/ViewControllers/RealCalendarEventVC/RealCalendarEventUIView.swift
+++ blockOffV4/ViewControllers/RealCalendarEventVC/RealCalendarEventUIView.swift
@@ -33,10 +33,45 @@ struct RealCalendarEventUIView: View {
                             .foregroundColor(.gray)
                         Spacer()
                     }
+                    CalendarItemRow(title:"All-day", item: ekEvent.isAllDay.description, isFirst: true)
                     CalendarItemRow(title:"Calendar", item: ekEvent.calendar.title)
+                    CalendarItemRow(title:"Show As", item: getStatus(ekEvent.availability.rawValue))
+                    CalendarItemRow(title:"Alert", item: ekEvent.alarms?.description ?? "none")
+                    CalendarItemRow(title: "Location", item: ekEvent.location ?? "none")
+                    CalendarItemRow(title: "Notes", item: ekEvent.notes ?? "none")
+                    ParticipantsListView(ekEvent: ekEvent)
                     
                 }.padding([.leading, .trailing])
             }
+        }
+    }
+    
+    private func getStatus(_ status: Int) -> String {
+        let status = Availability(rawValue: status)
+        return status?.displayText ?? ""
+    }
+}
+
+struct ParticipantsListView: View {
+    let ekEvent: EKEvent
+    
+    var body: some View {
+     
+        VStack(alignment: .leading) {
+            Text("Participants")
+                .font(.body)
+            if let attendees = ekEvent.attendees {
+                ForEach(attendees, id: \.self) { attendee in
+                    HStack {
+                        Spacer()
+                        Text(attendee.name ?? "")
+                            .font(.body)
+                            .foregroundColor(.gray)
+                    }
+                }
+            }
+            Divider()
+                .padding(.top, 0)
         }
     }
 }
@@ -44,11 +79,14 @@ struct RealCalendarEventUIView: View {
 struct CalendarItemRow: View {
     let title: String
     let item: String
+    var isFirst: Bool = false
     
     var body: some View {
         VStack {
-            Divider()
-                .padding(.bottom, 4)
+            if isFirst {
+                Divider()
+                    .padding(.bottom,2)
+            }
             HStack(alignment:.bottom) {
                 Text(title)
                     .font(.body)
