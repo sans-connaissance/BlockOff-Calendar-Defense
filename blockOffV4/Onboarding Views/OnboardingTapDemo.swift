777e4dee8d555b28eb2cfafc0a3cea92a1500492
David Malicke <davemalicke@gmail.com>
2023-04-11 23:21:37 -0400

    Add new screenshot

--- blockOffV4/Onboarding Views/OnboardingTapDemo.swift
+++ blockOffV4/Onboarding Views/OnboardingTapDemo.swift
@@ -11,7 +11,7 @@ struct OnboardingTapDemo: View {
     var body: some View {
         VStack {
             VStack(alignment: .center) {
-                Image("screenshot")
+                Image("screenshot2")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .padding()
